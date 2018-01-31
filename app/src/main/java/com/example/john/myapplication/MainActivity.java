2e2b357de0b27b88c99c31983b1ccbcd50e0edcf
passivemonster <johnkaramchand1@gmail.com>
2018-01-31 17:51:36 +0530

    first display and xml

--- app/src/main/java/com/example/john/myapplication/MainActivity.java
+++ app/src/main/java/com/example/john/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.john.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView sudoTitle ;
+    TextView sudoInfo ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sudoTitle = findViewById(R.id.sudoTitle) ;
+        sudoInfo = findViewById(R.id.sudoInfo) ;
     }
 }
